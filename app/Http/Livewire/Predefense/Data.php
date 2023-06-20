db0ea407cc40ae945bb978f0629e3d629cef7771
zahranrr4-aja <muhammadzahrane122@gmail.com>
2023-06-20 16:57:28 +0700

    Commit_Zahran

--- app/Http/Livewire/Predefense/Data.php
+++ app/Http/Livewire/Predefense/Data.php
@@ -4,16 +4,52 @@ namespace App\Http\Livewire\Predefense;
 
 use Livewire\Component;
 use App\Models\Upcoming\Event;
+use Livewire\WithPagination;
 
 class Data extends Component
 {
-    public $events,$event;  
+    use WithPagination;
+    protected $listeners=['type'];
+    public $events, $event;
+    public $perPage = 3;
+    public $page = 1;
+    public $numbering;
+
     public function mount()
     {
-        $this->events=Event::all();
+        
     }
+
     public function render()
     {
-        return view('livewire.predefense.data');
+        $this->filterData();
+
+        return view('livewire.predefense.data', [
+            'events' => $this->events
+        ]);
     }
+
+    private function filterData()
+    {
+        $this->events = Event::where('event_type', 1)
+            ->orderBy('event_date', 'desc')
+            ->skip(($this->page - 1) * $this->perPage)
+            ->take($this->perPage)
+            ->get();
+        
+            $this->numbering = ($this->page - 1) * $this->perPage + 1;    
+                }
+                public function nextPage()
+            {
+                if ($this->events->count() == $this->perPage) {
+                    $this->page++;
+                }
+            }
+
+            public function previousPage()
+            {
+                if ($this->page > 1) {
+                    $this->page--;
+                }
+            }
 }
